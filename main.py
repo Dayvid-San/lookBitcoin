5fb9dd15bee5950e1d1490eaf768ab42821dbe41
Dayvid Santana <dayvidcomerce@gmail.com>
2024-04-09 19:05:56 -0300

    feat: add window

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
 from time import sleep
-import os
+import winsound
+import tkinter as tk
 
 
 URL_API = "https://api.coindesk.com/v1/bpi/currentprice/USD.json"
-VALOR_LIMITE = int(input("Qual o valor mínimo da cotação?\n"))
+VALOR_LIMITE = float(input("Qual o valor mínimo da cotação?\n"))
 INTERVALO = 60
-ARQUIVO_SOM = "ativado.wav"
+ARQUIVO_SOM = "bitcoin.wav"
 
 def obter_cotacao():
     resposta = requests.get(URL_API)
@@ -14,15 +15,24 @@ def obter_cotacao():
     cotacao = dados["bpi"]["USD"]["rate_float"]
     return cotacao
 
+def mostrar_janela():
+    janela = tk.Tk()
+    janela.title("Bitcoins!!!")
+    janela.geometry("500x250")
+    label = tk.Label(text="Hora de comprar Bitcoins!", font=("Arial", 20))
+    label.pack()
+    janela.mainloop()
+
 while True:
     cotacao_atual = obter_cotacao()
 
-    if cotacao_atual < VALOR_LIMITE:
+    if cotacao_atual > VALOR_LIMITE:
         print('*'*30)
         print(f"Alerta! Cotação do Bitcoin ultrapassou o limite de {VALOR_LIMITE:.2f} USD!")
         print(f"Cotação atual: {cotacao_atual:.2f} USD")
 
-        os.system("start " + os.path.join(os.getcwd(), "bitcoin.mp3"))
+        winsound.PlaySound(ARQUIVO_SOM, winsound.SND_FILENAME)
+        mostrar_janela()
 
         break  
 
